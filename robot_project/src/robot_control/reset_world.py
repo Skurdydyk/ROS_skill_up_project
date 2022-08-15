9859c0e017d62555989195a825feee3cb6ec1799
skur <Sviat.Kurdydyk@teck.com>
2022-08-15 22:50:29 +0300

    Updated integration test

--- robot_project/src/robot_control/reset_world.py
+++ robot_project/src/robot_control/reset_world.py
@@ -2,9 +2,22 @@
 
 import rospy
 from std_srvs.srv import Empty, EmptyRequest
+from gazebo_msgs.srv import DeleteModel
 
-rospy.init_node('reset_world_client')
-rospy.wait_for_service('/gazebo/reset_world')
-reset_world_service = rospy.ServiceProxy('/gazebo/reset_world', Empty)
-srv_msg = EmptyRequest()
-result = reset_world_service(srv_msg)
+
+def reset_world():
+    rospy.init_node('reset_world_client')
+    rospy.wait_for_service('/gazebo/reset_world')
+    reset_world_service = rospy.ServiceProxy('/gazebo/reset_world', Empty)
+    srv_msg = EmptyRequest()
+    reset_world_service(srv_msg)
+
+
+def delete_model(model_name):
+    del_model_prox = rospy.ServiceProxy('gazebo/delete_model', DeleteModel)
+    rospy.wait_for_service('gazebo/delete_model')
+    del_model_prox(model_name)
+
+
+reset_world()
+delete_model('construction_barrel')
--- robot_project/test/helpers.py
+++ robot_project/test/helpers.py
@@ -1,7 +1,10 @@
 import rospy
+import rospkg
+import actionlib
 
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-import actionlib
+from gazebo_msgs.srv import GetModelState, SpawnModel
+from geometry_msgs.msg import Pose
 
 
 def feedback_callback(feedback):
@@ -31,3 +34,33 @@ def send_goal(pos_x, pos_y, pos_z, orient_w):
     client.wait_for_result()
 
     rospy.logwarn("[Result] State: %d" % (client.get_state()))
+
+
+def spawn_model(model_name):
+    try:
+        initial_pose = Pose()
+        initial_pose.position.x = 3
+        initial_pose.position.y = 0
+        initial_pose.position.z = 0.2
+
+        package_path = rospkg.RosPack().get_path("robot_project")
+
+        with open(f'{package_path}/models/{model_name}/model.sdf', 'r') as xml_file:
+            model_xml = xml_file.read().replace('\n', '')
+
+        spawn_model_prox = rospy.ServiceProxy('gazebo/spawn_sdf_model', SpawnModel)
+        spawn_model_prox(model_name, model_xml, '', initial_pose, 'world')
+    except rospy.ServiceException as e:
+        rospy.logwarn('Spawn Model service call failed: {0}'.format(e))
+
+
+def get_model_position(model_name):
+    resp_coordinates = None
+
+    try:
+        model_coordinates = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
+        resp_coordinates = model_coordinates(model_name, '')
+    except rospy.ServiceException as e:
+        rospy.logwarn('Get Model State service call failed: {0}'.format(e))
+
+    return resp_coordinates
