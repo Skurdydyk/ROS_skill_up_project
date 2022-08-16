c1d8e1bbf0f4b0fd737b9e0529e0bc635ab65cd8
skur <Sviat.Kurdydyk@teck.com>
2022-08-16 14:29:45 +0300

    Refactored code

--- robot_project/src/robot_project/reset_world.py
+++ robot_project/src/robot_project/reset_world.py
@@ -1,24 +1,9 @@
 #! /usr/bin/env python
 
-import rospy
-from std_srvs.srv import Empty, EmptyRequest
-from gazebo_msgs.srv import DeleteModel
-
-
-def reset_world():
-    rospy.init_node('reset_world_client')
-    rospy.wait_for_service('/gazebo/reset_world')
-    reset_world_service = rospy.ServiceProxy('/gazebo/reset_world', Empty)
-    srv_msg = EmptyRequest()
-    reset_world_service(srv_msg)
-
-
-def delete_model(model_name):
-    del_model_prox = rospy.ServiceProxy('gazebo/delete_model', DeleteModel)
-    rospy.wait_for_service('gazebo/delete_model')
-    del_model_prox(model_name)
-
+from helpers import Action
 
 if __name__ == '__main__':
-    reset_world()
-    delete_model('construction_barrel')
+    action = Action()
+
+    action.reset_world()
+    action.delete_model('construction_barrel')
--- robot_project/test/avoid_obstacles_robot_integration_test.py
+++ robot_project/test/avoid_obstacles_robot_integration_test.py
@@ -4,7 +4,7 @@ import unittest
 import rostest
 import rospy
 
-from helpers import send_goal, spawn_model, get_model_position
+from robot_project.helpers import Action
 
 PKG = 'robot_project'
 NAME = 'avoid_obstacle_robot_integration_test'
@@ -13,26 +13,27 @@ NAME = 'avoid_obstacle_robot_integration_test'
 class TestRobotControl(unittest.TestCase):
     def setUp(self):
         rospy.init_node('test_node')
+        self.action = Action()
 
     def test_avoid_obstacle(self):
         # send the first move base goal to move
-        send_goal(2, 0.0, 0.0, 0.2)
+        self.action.send_goal(2, 0.0, 0.0, 0.2)
         # get coordinates of the robot before avoiding an obstacle
-        coordinates_robot = get_model_position('my_robot')
+        coordinates_robot = self.action.get_model_position('my_robot')
 
         # check the position of the robot
         self.assertTrue(1.3 <= coordinates_robot.pose.position.x <= 1.6)
         self.assertTrue(-0.3 <= coordinates_robot.pose.position.y <= 0.3)
 
         # spawning an obstacle directly in front of the robot
-        spawn_model('construction_barrel', 3, 0, 0.2)
+        self.action.spawn_model('construction_barrel', 3, 0, 0.2)
 
         # send second move base goal
-        send_goal(6, 0.0, 0.0, 0.2)
+        self.action.send_goal(6, 0.0, 0.0, 0.2)
 
         # get coordinates robot and obstacle after avoiding obstacle
-        coordinates_robot = get_model_position('my_robot')
-        coordinates_obstacle = get_model_position('construction_barrel')
+        coordinates_robot = self.action.get_model_position('my_robot')
+        coordinates_obstacle = self.action.get_model_position('construction_barrel')
 
         self.assertTrue(5 <= coordinates_robot.pose.position.x <= 6)
         self.assertTrue(-0.3 <= coordinates_robot.pose.position.y <= 0.3)
